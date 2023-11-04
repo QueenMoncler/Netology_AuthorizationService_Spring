b00618b3d1b5dab3f87391f2e0ffc187dffd6afd
queen <Queenmoncler@gmail.com>
2023-11-04 15:52:31 +0300

    Initial Commit

---
+++ src/main/java/com/example/netology_authorizationservice_spring/NetologyAuthorizationServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.netology_authorizationservice_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetologyAuthorizationServiceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetologyAuthorizationServiceSpringApplication.class, args);
+    }
+
+}
